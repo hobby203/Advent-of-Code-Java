12788f49322280bb1e689937287bc93a0abd96be
Edward Hobbs <edward24hobbs@outlook.com>
2015-12-04 18:05:47 +0000

    Beginnings of code for day 2

---
+++ Advent of Code/src/Day2.java
@@ -0,0 +1,19 @@
+/**
+ * Code for day 2 of adventofcode
+ */
+public class Day2 {
+
+    public Day2(){
+    }
+
+    public int total_wrapping_paper(String presents){
+        int total_paper = 0;
+        String[] groups = presents.split("\n");
+        for (String group: groups){
+            String[] present = group.split("x");
+
+        }
+
+        return total_paper;
+    }
+}
